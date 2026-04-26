a89bc66426af80207f003de6f89875b72be8f455
MuhammedWael9991 <mw26200400@gmail.com>
2026-04-26 18:54:54 +0300

    add IBM font family implementation

--- app/src/main/java/com/salamhack/presentation/shared/designSystem/textStyle/Font.kt
+++ app/src/main/java/com/salamhack/presentation/shared/designSystem/textStyle/Font.kt
@@ -3,12 +3,13 @@ package com.salamhack.presentation.shared.designSystem.textStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import com.salamhack.R
 
-//val ibm = FontFamily(
-//    Font(R.font.ibmplexsansarabic_regular, weight = FontWeight.Normal),
-//    Font(R.font.ibmplexsansarabic_bold, weight = FontWeight.Bold),
-//    Font(R.font.ibmplexsansarabic_extralight, weight = FontWeight.ExtraLight),
-//    Font(R.font.ibmplexsansarabic_light, weight = FontWeight.Light),
-//    Font(R.font.ibmplexsansarabic_medium, weight = FontWeight.Medium),
-//    Font(R.font.ibmplexsansarabic_semibold, weight = FontWeight.SemiBold),
-//)
+val ibm = FontFamily(
+    Font(R.font.ibmplexsansarabic_regular, weight = FontWeight.Normal),
+    Font(R.font.ibmplexsansarabic_bold, weight = FontWeight.Bold),
+    Font(R.font.ibmplexsansarabic_extralight, weight = FontWeight.ExtraLight),
+    Font(R.font.ibmplexsansarabic_light, weight = FontWeight.Light),
+    Font(R.font.ibmplexsansarabic_medium, weight = FontWeight.Medium),
+    Font(R.font.ibmplexsansarabic_semibold, weight = FontWeight.SemiBold),
+)
