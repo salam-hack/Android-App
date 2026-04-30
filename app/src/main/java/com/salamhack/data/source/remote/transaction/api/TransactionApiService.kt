8f9cb83d5b8a2d9f1cddc5039d8f9004f3e6ee9b
MuhammedWael9991 <mw26200400@gmail.com>
2026-05-01 02:47:02 +0300

    feat: add parseTransactionAi endpoint to TransactionApiService for AI transaction parsing

--- app/src/main/java/com/salamhack/data/source/remote/transaction/api/TransactionApiService.kt
+++ app/src/main/java/com/salamhack/data/source/remote/transaction/api/TransactionApiService.kt
@@ -2,6 +2,8 @@ package com.salamhack.data.source.remote.transaction.api
 
 import com.salamhack.data.source.remote.transaction.dto.AddTransactionRequestDto
 import com.salamhack.data.source.remote.transaction.dto.AddTransactionResponseDto
+import com.salamhack.data.source.remote.transaction.dto.ParseTransactionAiRequestDto
+import com.salamhack.data.source.remote.transaction.dto.ParseTransactionAiResponseDto
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -9,4 +11,7 @@ import retrofit2.http.POST
 interface TransactionApiService {
     @POST("api/transactions/add-manual")
     suspend fun addManualTransaction(@Body request: AddTransactionRequestDto): AddTransactionResponseDto
+
+    @POST("api/transactions/parse-ai")
+    suspend fun parseTransactionAi(@Body request: ParseTransactionAiRequestDto): ParseTransactionAiResponseDto
 }
