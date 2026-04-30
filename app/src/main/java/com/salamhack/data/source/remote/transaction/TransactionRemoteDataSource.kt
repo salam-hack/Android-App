28d5b2b9c3b58daeddd58b5f9d5c68d76331b694
MuhammedWael9991 <mw26200400@gmail.com>
2026-05-01 02:47:07 +0300

    feat: add parseTransactionAi method to TransactionRemoteDataSource for AI transaction parsing

--- app/src/main/java/com/salamhack/data/source/remote/transaction/TransactionRemoteDataSource.kt
+++ app/src/main/java/com/salamhack/data/source/remote/transaction/TransactionRemoteDataSource.kt
@@ -3,9 +3,12 @@ package com.salamhack.data.source.remote.transaction
 import com.salamhack.data.source.remote.transaction.api.TransactionApiService
 import com.salamhack.data.source.remote.transaction.dto.AddTransactionRequestDto
 import com.salamhack.data.source.remote.transaction.dto.AddTransactionResponseDto
+import com.salamhack.data.source.remote.transaction.dto.ParseTransactionAiRequestDto
+import com.salamhack.data.source.remote.transaction.dto.ParseTransactionAiResponseDto
 
 interface TransactionRemoteDataSource {
     suspend fun addManualTransaction(request: AddTransactionRequestDto): AddTransactionResponseDto
+    suspend fun parseTransactionAi(request: ParseTransactionAiRequestDto): ParseTransactionAiResponseDto
 }
 
 class TransactionRemoteDataSourceImpl(
@@ -14,5 +17,9 @@ class TransactionRemoteDataSourceImpl(
 
     override suspend fun addManualTransaction(request: AddTransactionRequestDto): AddTransactionResponseDto {
         return api.addManualTransaction(request)
+    }
+
+    override suspend fun parseTransactionAi(request: ParseTransactionAiRequestDto): ParseTransactionAiResponseDto {
+        return api.parseTransactionAi(request)
     }
 }
