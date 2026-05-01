9e626f1e1a6cb95de2267c01b2b57b5af4ecfd04
MuhammedWael9991 <mw26200400@gmail.com>
2026-05-01 04:20:18 +0300

    feat: add getChatHistory method to ChatApiService for retrieving chat history

--- app/src/main/java/com/salamhack/data/source/remote/chatBot/api/ChatApiService.kt
+++ app/src/main/java/com/salamhack/data/source/remote/chatBot/api/ChatApiService.kt
@@ -1,5 +1,7 @@
 package com.salamhack.data.source.remote.chatBot.api
 
+import com.salamhack.data.source.remote.chatBot.dto.ChatListRequestDto
+import com.salamhack.data.source.remote.chatBot.dto.ChatListResponseDto
 import com.salamhack.data.source.remote.chatBot.dto.CreateChatRequestDto
 import com.salamhack.data.source.remote.chatBot.dto.CreateChatResponseDto
 import com.salamhack.data.source.remote.chatBot.dto.SendMessageRequestDto
@@ -13,4 +15,7 @@ interface ChatApiService {
 
     @POST("api/chat/send")
     suspend fun sendMessage(@Body request: SendMessageRequestDto): SendMessageResponseDto
+
+    @POST("api/chat/list")
+    suspend fun getChatHistory(@Body request: ChatListRequestDto): ChatListResponseDto
 }
